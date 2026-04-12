ed804c7d25a979678368ac0ce2a5c49816adbfce
Kahitoz <kshitiz.sinha.025@gmail.com>
2026-04-12 17:45:38 +0530

    feat: implemented the fastapi server

---
+++ backend/app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+from app.configs.qwen_config import call_tts
+from app.configs.config import get_llm_providers, get_tts_providers
+
+app = FastAPI()
+
+class VoiceBody(BaseModel):
+    name: str
+    language: str
+    text: str
+    instruct: str
+
+@app.get("/health")
+def health_check():
+    return {"status": 200, "message": "OK"}
+
+@app.get("/info")
+def get_info():
+    llm = get_llm_providers()
+    tts = get_tts_providers()
+    
+    return {'llm':llm, 'tts':tts}
+    
+
+@app.post("/tts")
+def get_voice(body: VoiceBody):
+    audio_buffer = call_tts(
+        text=body.text,
+        language=body.language,
+        speaker=body.name,
+        instruct=body.instruct,
+    )
+    return StreamingResponse(audio_buffer, media_type="audio/wav")
